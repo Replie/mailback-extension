f0928afe10f4b3166b82d63e094517fbe374ac2a
Eran Lunenfeld <eranl@paykey.com>
2018-05-18 15:36:08 +0300

    suggestions addedgit push

--- js/main.js
+++ js/main.js
@@ -19,8 +19,8 @@ var main = function(){
     selector: 'div.Jd-axF:first-child'
   });
   gmail.observe.register('compose_select', {
-    class: 'ip',
-    sub_selector: 'div.M9'
+    class: 'M9',
+    selector: 'div.M9:first-child'
   });
   console.log('Hello,', gmail.get.user_email())
   // $.get('http://193.106.55.110:8080/_predict', {
@@ -31,14 +31,21 @@ var main = function(){
   //     });
   gmail.observe.on("compose", function(compose, type) {
     console.log("on compose");
-  
+    window.setTimeout( function() { 
+      suggestions = "<div class=\"btn-group\"><button style=\"border: solid 2px #a51c93;padding: 10px;margin-right: 10px;-moz-border-radius: 50px;-webkit-border-radius: 50px;border-radius: 50px;\" type=\"button\" class=\"btn btn-default btn-xs\">Yes I would Love to Come!</button><button style=\"border: solid 2px #a51c93;padding: 10px;margin-right: 10px;-moz-border-radius: 50px;-webkit-border-radius: 50px;border-radius: 50px;\" type=\"button\" class=\"btn btn-default btn-xs\">Yes I will be able to make it</button><button style=\"border: solid 2px #a51c93;padding: 10px;margin-right: 10px;-moz-border-radius: 50px;-webkit-border-radius: 50px;border-radius: 50px;\" type=\"button\" class=\"btn btn-default btn-xs\">Sorry, I wont be able to make it</button></div></div>";
+      compose.body(suggestions + "<br/><br/>"); 
+      // gmail.tools.add_compose_button(compose, "<div>suggestion</div>", function() {
+      //   // Code here
+      // }, 'Custom Style Classes');
+    }, 0); 
     // console.log(gmail.dom.email_body()[0].textContent)
     suggestions = "<div class=\"btn-group\"><button style=\"border: solid 2px #a51c93;padding: 10px;margin-right: 10px;-moz-border-radius: 50px;-webkit-border-radius: 50px;border-radius: 50px;\" type=\"button\" class=\"btn btn-default btn-xs\">Yes I would Love to Come!</button><button style=\"border: solid 2px #a51c93;padding: 10px;margin-right: 10px;-moz-border-radius: 50px;-webkit-border-radius: 50px;border-radius: 50px;\" type=\"button\" class=\"btn btn-default btn-xs\">Yes I will be able to make it</button><button style=\"border: solid 2px #a51c93;padding: 10px;margin-right: 10px;-moz-border-radius: 50px;-webkit-border-radius: 50px;border-radius: 50px;\" type=\"button\" class=\"btn btn-default btn-xs\">Sorry, I wont be able to make it</button></div></div>";
     // compose[0].body("suggestions")
     console.log(compose);
     // console.log(compose.$el[0].body('Hello'));
-    gmail.dom.composes()[0].body("suggestions")
-    console.log("add text");
+    // gmail.dom.composes()[0].body("suggestions")
+    // console.log("add text");
+    console.log(compose.dom.body);
     // gmail.dom.composes()[0].body("test asdfa sdf kads;fkjasdkfjas;dfkjasdkj as;lkdfjalskdf j;lkasdjfl;kasjdfl;kajsdfkl;jasd fkjas;ldfjk;asldfjl;kjasdfj;asd;lfkjl;kajsdfkjkljasdfkjaslk;dfjlkajsdflksj");
   });
 
@@ -51,7 +58,7 @@ var main = function(){
   gmail.observe.on('compose_select', function(match) {
     console.log('Compose select',match);
     suggestions = "<div class=\"btn-group\"><button style=\"border: solid 2px #a51c93;padding: 10px;margin-right: 10px;-moz-border-radius: 50px;-webkit-border-radius: 50px;border-radius: 50px;\" type=\"button\" class=\"btn btn-default btn-xs\">Yes I would Love to Come!</button><button style=\"border: solid 2px #a51c93;padding: 10px;margin-right: 10px;-moz-border-radius: 50px;-webkit-border-radius: 50px;border-radius: 50px;\" type=\"button\" class=\"btn btn-default btn-xs\">Yes I will be able to make it</button><button style=\"border: solid 2px #a51c93;padding: 10px;margin-right: 10px;-moz-border-radius: 50px;-webkit-border-radius: 50px;border-radius: 50px;\" type=\"button\" class=\"btn btn-default btn-xs\">Sorry, I wont be able to make it</button></div></div>";
-    gmail.dom.composes()[0].body("suggestions")
+    gmail.dom.composes()[0].body(suggestions)
   });
 
  
