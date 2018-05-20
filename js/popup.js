c342653752711d88591eb0e21c153831cc1b090d
Tal Machani <tal.machani@idomoo.com>
2018-05-20 19:36:48 +0300

    logos + options and pop up

---
+++ js/popup.js
@@ -0,0 +1,14 @@
+<DOCTYPE html> </DOCTYPE>
+<html>
+
+<body>
+  <button id="changeColor"></button>
+  <script src="jquery-3.3.1.min.js"></script>
+  <script src="popup.js"></script>
+  <div id="header">
+      <ul class="tabs">
+      </ul>
+  </div>
+</body>
+
+</html>
