87d164029a9b5e7c4082843f42ae4c3d0e175eed
Tal Machani <tal.machani@idomoo.com>
2018-05-21 19:06:00 +0300

    now works only in reply

--- js/main.js
+++ js/main.js
@@ -19,32 +19,34 @@ var main = function () {
   $.post(predict_url, {
     seq_str: btoa("input_sentence"),
   })
-    .done(function (msg) { 
+    .done(function (msg) {
       console.log("Go predictions")
       suggestions = msg.data.results;
     })
     .fail(function (xhr, status, error) {
-      console.log("Failed To predict, status: "+ status + " Error: " + error)
-      suggestions = ["Ok, i'm on it.","Sorry No can Do.","Sure, Thank's"];
+      console.log("Failed To predict, status: " + status + " Error: " + error)
+      suggestions = ["Ok, i'm on it.", "Sorry No can Do.", "Sure, Thank's"];
     });
-    
-    gmail.observe.on("compose", function (compose, type) {
-    window.setTimeout(function () {
-      let suggestions_html = '<div id="mb_suggest1" class="mb-button-group btn-group">' +
-      '<button type="button" id="mb_button" class="btn mb-button btn-raised">'
-       + suggestions[0] +'</button>' +
-      '<button type="button" id="mb_button2" class="btn mb-button btn-raised">'
-       + suggestions[1] + '</button>' + 
-      '<button type="button" id="mb_button3" class="btn mb-button btn-raised">'
-       + suggestions[2] + '</button>' +
-      '</div>';
-      compose.body("<br/><br/>" + suggestions_html);
-      $(document).ready(function() {
-        $(".mb-button").on( "click", function() {
-        compose.body($( this ).text() + "<br/><br/>");
+
+  gmail.observe.on("compose", function (compose, type) {
+    if (type === 'reply') {
+      window.setTimeout(function () {
+        let suggestions_html = '<div id="mb_suggest1" class="mb-button-group btn-group">' +
+          '<button type="button" id="mb_button" class="btn mb-button btn-raised">'
+          + suggestions[0] + '</button>' +
+          '<button type="button" id="mb_button2" class="btn mb-button btn-raised">'
+          + suggestions[1] + '</button>' +
+          '<button type="button" id="mb_button3" class="btn mb-button btn-raised">'
+          + suggestions[2] + '</button>' +
+          '</div>';
+        compose.body("<br/><br/>" + suggestions_html);
+        $(document).ready(function () {
+          $(".mb-button").on("click", function () {
+            compose.body($(this).text() + "<br/><br/>");
+          });
         });
-      });
-    }, 0);
+      }, 0);
+    }
   });
 }
 refresh(main);
