9738f32e2027da871afde24809a9766adc7cbdc3
Eran Lunenfeld <eranl@paykey.com>
2018-05-23 22:41:49 +0300

    fix request

--- js/main.js
+++ js/main.js
@@ -15,25 +15,27 @@ function refresh(f) {
 var main = function () {
   // NOTE: Always use the latest version of gmail.js from
   gmail = new Gmail();
-  console.log('MailBack is of for: ', gmail.get.user_email())
+  console.log('MailBack is of for: ', gmail.get.user_email());
+  var email = gmail.dom.email($('div.adn')); 
+  var body = email.data().content_plain;
+  console.log('MailBack for content: ', body);
   $.ajax({
     url: predict_url,
-    type: 'post',
-    data: { seq_str: btoa("input_sentence") },
+    type: 'get',
+    data: { seq_str: btoa(body) },
     headers: {
-        "Content-Type": "application/json"
+      "Content-Type": "application/json"
     },
     dataType: 'json',
-    success: function (data){
-      console.log("Go predictions")
-      suggestions = msg.data.results;
+    success: function (data) {
+      console.log("Go predictions");
+      suggestions = data.data.results;
     },
-    error: function (data){
+    error: function (data) {
       console.log("Failed To predict, status: " + status + " Error: " + error)
-      suggestions = ["Ok, i'm on it.", "Sorry No can Do.", "Sure, Thank's"];  
+      suggestions = ["Ok, i’m on it.", "Sorry No can Do.", "Sure, Thank’s"];
     }
-
-    });
+  });
 
 
   // $.ajax({url:predict_url, {
